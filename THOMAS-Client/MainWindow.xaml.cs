36adecd990db175a0515c9b6d99af6abf900183c
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2016-12-19 00:13:40 +0100

    Namespace angepasst

--- THOMAS-Client/MainWindow.xaml.cs
+++ THOMAS-Client/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace THOMAS_Client
+namespace THOMASClient
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
